e81be92ca24efb8d6cbe6d48e9ddda4d5455a45d
Mikapgunady <mika.pg01@yahoo.com>
2022-07-26 15:11:42 +0700

    first commit

---
+++ pages/Employee/columns.js
@@ -0,0 +1,72 @@
+import Employee from ".";
+
+export const Columns = [
+    {
+        Header : 'Id',
+        accessor : 'id'
+    },
+    {
+        Header : 'Employee',
+        accessor : 'name'
+    },
+    {
+        Header : 'Organization',
+        accessor : 'organization'
+    },
+    {
+        Header : 'Job Level',
+        accessor : 'jobLevel'
+    },
+    {
+        Header : 'Employment Status',
+        accessor : 'employementStatus'
+    },
+    {
+        Header : 'Join Date',
+        accessor : 'joinDate'
+    },
+    {
+        Header : 'End Date',
+        accessor : 'endDate'
+    },
+    {
+        Header : 'Resign Date',
+        accessor : 'resignDate'
+    },
+    {
+        Header : 'Barcode',
+        accessor : 'barcode'
+    },
+    {
+        Header : 'Email',
+        accessor : 'email'
+    },
+    {
+        Header : 'Birth Date',
+        accessor : 'birthDate'
+    },
+    {
+        Header : 'Birth Place ',
+        accessor : 'birthPlace'
+    },
+    {
+        Header : 'Address',
+        accessor : 'address'
+    },
+    {
+        Header : 'Mobile Phone',
+        accessor : 'mobilePhone'
+    },
+    {
+        Header : 'Religion ',
+        accessor : 'religion'
+    },
+    {
+        Header : 'Gender',
+        accessor : 'gender'
+    },
+    {
+        Header : 'Marital Status',
+        accessor : 'maritalStatus'
+    },
+];
